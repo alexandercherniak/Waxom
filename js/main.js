1c679d16831c4f0a6ad441d5053f76f731d2088f
alexandercherniak <padre.12028@gmail.com>
2019-08-24 10:07:38 +0300

    ищу иконки

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
